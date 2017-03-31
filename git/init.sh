ff493ec8b45cb62bef2ff933ef5f26450d206e6b
Nenad Novakovic <n.dvLden@gmail.com>
2017-04-01 01:17:36 +0200

    git was not neccessery - already contained within symlinks

--- git/init.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-git config --global user.name "Nenad Novakovic"
-git config --global user.email "n.dvLden@gmail.com"
-git config --global core.editor "subl -n -w"
-git config --global color.ui true
-git config --global credential.helper osxkeychain
